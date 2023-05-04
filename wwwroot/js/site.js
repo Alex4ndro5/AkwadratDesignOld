a5061879c1d72759a03af0d385e6814f2339cff9
Alex4ndro5 <alek.folfas@gmail.com>
2023-05-04 15:09:05 +0200

    html,css,js oraz azure app service

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.onload = function () {
+    window.addEventListener('scroll', function (e) {
+        if (window.pageYOffset > 100) {
+            this.document.querySelector('header').classList.add('is-hidden');
+            this.document.querySelector('.side-menu-container').classList.add('is-visible');
+        }
+        else {
+            this.document.querySelector('header').classList.remove('is-hidden');
+            this.document.querySelector('.side-menu-container').classList.remove('is-visible');
+        }
+    });
+}
+// Get all the links in the navigation menu
+const navLinks = document.querySelectorAll('nav a');
 
-// Write your JavaScript code.
+// Add a click event listener to each link
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Get the target section's ID from the link's href attribute
+        const targetId = link.getAttribute('href');
+
+        // Use the target section's ID to get the corresponding element
+        const targetElement = document.querySelector(targetId);
+
+        // Calculate the distance from the top of the page to the target element
+        const targetOffset = targetElement.offsetTop;
+
+        // Use the window.scrollTo() method to smoothly scroll to the target element
+        window.scrollTo({
+            top: targetOffset,
+            behavior: 'smooth'
+        });
+    });
+});
