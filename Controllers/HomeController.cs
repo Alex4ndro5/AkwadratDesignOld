07472851c64de7f95510c5c51bbd444cb09b496b
emili <emiliasroka.a@gmail.com>
2023-05-13 12:09:21 +0200

    iactionresult w homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,16 +23,30 @@ namespace AkwadratDesign.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Collaborations()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult MenuPartial()
-        {
-            return PartialView("_Menu", _logger);
         }
     }
 }
