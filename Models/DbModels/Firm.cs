65dfb482118823d213cfd902bbef6a6aa1c13734
Natalia804 <nlys@student.agh.edu.pl>
2023-05-13 14:59:07 +0200

    Firm Model 1

--- Models/DbModels/Firm.cs
+++ Models/DbModels/Firm.cs
@@ -1,6 +1,18 @@
-﻿namespace AkwadratDesign.Models.DbModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AkwadratDesign.Models.DbModels
 {
     public class Firm
     {
+        [Key]  
+        public int Id { get; set; }
+        [Required]
+        public string FirmName { get; set; }
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
     }
 }
