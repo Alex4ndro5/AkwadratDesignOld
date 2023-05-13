9b79d8d7eef1386c8881f0be061533fca0e5b779
Alex4ndro5 <alek.folfas@gmail.com>
2023-05-13 14:23:46 +0200

    revive

--- Models/DbModels/Admin.cs
+++ Models/DbModels/Admin.cs
@@ -4,11 +4,5 @@ namespace AkwadratDesign.Models.DbModels
 {
     public class Admin
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Login { get; set; }
-        [Required]
-        public string Password { get; set; }
     }
 }
