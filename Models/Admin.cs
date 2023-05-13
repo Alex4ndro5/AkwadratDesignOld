65be5ff25cb2fa5d9f9faa56be3a705d3727e12e
Natalia804 <nlys@student.agh.edu.pl>
2023-05-13 11:01:25 +0200

    Admin model

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,6 +1,14 @@
-﻿namespace AkwadratDesign.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AkwadratDesign.Models
 {
     public class Admin
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }
