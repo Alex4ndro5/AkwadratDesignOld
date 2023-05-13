37ecbd831e110ec1f271ed97913da3289d0a2dc9
Natalia804 <nlys@student.agh.edu.pl>
2023-05-13 14:39:10 +0200

    Project fim

--- Models/DbModels/ProjectFirm.cs
+++ Models/DbModels/ProjectFirm.cs
@@ -2,5 +2,9 @@
 {
     public class ProjectFirm
     {
+        public int FirmsId { get; set; }
+        public int ProjectsId { get; set; }
+        public Firm Firm { get; set; } = null!;
+        public Project Project { get; set; } = null!;
     }
 }
