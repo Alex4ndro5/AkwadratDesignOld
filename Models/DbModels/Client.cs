5549670f5208ac5089bf54c15d8cfa8f72746753
Natalia804 <nlys@student.agh.edu.pl>
2023-05-13 14:51:04 +0200

    Client Model

--- Models/DbModels/Client.cs
+++ Models/DbModels/Client.cs
@@ -1,6 +1,20 @@
-﻿namespace AkwadratDesign.Models.DbModels
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AkwadratDesign.Models.DbModels
 {
-    public class Client
+    public class Client :IdentityUser
     {
+        [Key]
+        private int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]  
+        public string  Surname { get; set; }
+        [Required]  
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; } 
+
     }
 }
