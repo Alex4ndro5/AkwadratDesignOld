e2e15740f92c7660170c5d654b00e2602ddba50d
emili <emiliasroka.a@gmail.com>
2023-05-19 13:13:57 +0200

    już prawie prawie zapis projektu do bazy (zmiany w kontrolerze Admin i widoku Update)

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,8 @@
-﻿using AkwadratDesign.Services;
+﻿using AkwadratDesign.Models;
+using AkwadratDesign.Models.DbModels;
+using AkwadratDesign.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 
 namespace AkwadratDesign.Controllers
@@ -7,9 +10,11 @@ namespace AkwadratDesign.Controllers
     public class AdminController : Controller
     {
         private IAccountService accountService;
-        public AdminController(IAccountService _accountService)
+        private readonly DatabaseContext _context;
+        public AdminController(IAccountService _accountService, DatabaseContext context)
         {
             accountService = _accountService;
+            _context = context;
         }
         /// <summary>
         /// Default controller method, changes route to /admin
@@ -35,9 +40,150 @@ namespace AkwadratDesign.Controllers
             }
         }
 
+
+
+
+
+
+        // GET: Projects
+        public async Task<IActionResult> Portfolio()
+        {
+            return _context.Projects != null ?
+                        View(await _context.Projects.ToListAsync()) :
+                        Problem("Entity set 'DatabaseContext.Projects'  is null.");
+        }
+
+        // GET: Projects/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Projects == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // GET: Projects/Create
         public IActionResult Upload()
         {
-            return View();
+            return View(new Project());
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload([Bind("Id,Title,Description,Image,Type,TypeClient")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(project);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Projects == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Image,Type,TypeClient")] Project project)
+        {
+            if (id != project.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(project);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(project.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Upload));
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Projects == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Projects == null)
+            {
+                return Problem("Entity set 'DatabaseContext.Projects'  is null.");
+            }
+            var project = await _context.Projects.FindAsync(id);
+            if (project != null)
+            {
+                _context.Projects.Remove(project);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Upload));
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return (_context.Projects?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
